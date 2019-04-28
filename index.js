d45f545ec995ebe3b33a9a6edccaa87a558e5f38
Andrew Bagwell <abagwell90@gmail.com>
2019-04-28 11:07:16 -0400

    forgot port

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const google = require("./googlesheetapi")
 
+const port = process.env.PORT || 4000;
+
 const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,7 +15,9 @@ app.post('/', (req, res) => {
     google.writeAnswers(req.body);
     return res.send('submitted');
 })
+
 app.get('/', (req, res) => {
     return res.send('sWelcome to the AppBpo Server!');
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
